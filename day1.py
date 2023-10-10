382e5852cdf5ae92a71f0ad1abda0f9ce71d9f62
JCDWHIZ <jesseugboh@gmail.com>
2023-10-10 12:26:52 +0100

    training

---
+++ day1.py
@@ -0,0 +1,24 @@
+import math
+
+# number1_str = input("Enter a number")
+# number1_int = int(number1_str)
+# number2_str = input("Enter a string")
+# number2_int = int(number2_str)
+# sum = number1_int + number2_int
+# print(sum)
+
+raduis1_str = input("Enter a raduis ")
+raduis2_int = int(raduis1_str)
+circumference_int = 2 * math.pi * int(raduis2_int)
+area_int = math.pi * (raduis2_int ** 2)
+print("The circumference is", circumference_int, "The Area is:", area_int)
+# pi area
+
+
+
+
+# the circumferece of a circle given its raduis the mathtmatical formula is  == 2 * pi *raduis
+# area = pi * raduis
+# prompt for a raduis
+# apply the formula
+# print the result
